fef4c6be9807121a0cbee17ee1fc22ef10506645
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-08-28 16:53:37 +0900

    Security

--- src/main/java/behw6/schoolexam/entity/Student.java
+++ src/main/java/behw6/schoolexam/entity/Student.java
@@ -1,12 +1,11 @@
 package behw6.schoolexam.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
@@ -15,10 +14,14 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotEmpty(message = "이름은 필수항목입니다.")
     private String username;
     private String password;
     private String addr;
     private String phone;
+
+    @OneToMany
+    private List<Sugang> sugangs;
 
     private LocalDateTime date;
 }
